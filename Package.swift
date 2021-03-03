ab6af476cf0adcff7d3ad15ca0f0be7c83a19aa2
215eight <215eight@gmail.com>
2021-03-03 06:50:12 -0800

    Update Package definition

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        //.package(url: "git@github.com:215eight/DogWalkCoreData.git", .branch("main")),
+        .package(name: "SEDogWalkCoreData", url: "git@github.com:215eight/DogWalkCoreData.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DogWalkService",
-            dependencies: ["DogWalkCoreData"]),
+            dependencies: ["SEDogWalkCoreData"]),
         .testTarget(
             name: "DogWalkServiceTests",
             dependencies: ["DogWalkService"]),

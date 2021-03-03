9e960d719f35e7b9ab2f3c2a76e49bce9e995ee2
215eight <215eight@gmail.com>
2021-03-03 07:22:59 -0800

    Update package files to use https insteado of ssh

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SEDogWalkCoreData", url: "git@github.com:215eight/DogWalkCoreData.git", .branch("main")),
+        .package(name: "SEDogWalkCoreData", url: "https://github.com/215eight/DogWalkCoreData.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

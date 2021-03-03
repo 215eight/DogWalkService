db658a2121a7e39b8789344a419fe8f152d6e0bd
215eight <215eight@gmail.com>
2021-03-03 06:42:08 -0800

    Update package to a public repo

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "DogWalkService",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v11), .watchOS(.v4)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        //.package(url: "/Users/partyman21/Dev/Playground/CoreData_Chapter3/DogWalkCoreData", .branch("main")),
+        //.package(url: "git@github.com:215eight/DogWalkCoreData.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DogWalkService",
-            dependencies: []),
+            dependencies: ["DogWalkCoreData"]),
         .testTarget(
             name: "DogWalkServiceTests",
             dependencies: ["DogWalkService"]),
